f767444388eae7b62cf640c5e1dc68f6ff9ea650
qBlackbirdp <blackbird9711@gmail.com>
2024-09-05 16:49:14 +0900

    ChatMessage 클래스 관련 테스트

---
+++ src/main/java/com/example/exam/chat_24_09/ChatMessage.java
@@ -0,0 +1,21 @@
+package com.example.exam.chat_24_09;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class ChatMessage {
+    private long id;
+    private LocalDateTime createDate;
+    private String authorName;
+    private String content;
+
+    public ChatMessage(String authorName, String content) {
+        this(1, LocalDateTime.now(), authorName, content);
+    }
+
+}
