87e400ed87f93a687f80e0fa17b17d19845bd0c3
qBlackbirdp <blackbird9711@gmail.com>
2024-09-06 13:54:29 +0900

    message 작성시 작성자, 내용 JSON으로 전달

--- src/main/java/com/example/exam/chat_24_09/ChatController.java
+++ src/main/java/com/example/exam/chat_24_09/ChatController.java
@@ -1,5 +1,7 @@
 package com.example.exam.chat_24_09;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,20 +14,34 @@ public class ChatController {
 
     private List<ChatMessage> chatMessages = new ArrayList<>();
 
-    public record writeChatMessageResponse (long id) {
+//    @AllArgsConstructor
+//    @Getter
+//    public static class writeMessageRequest () {
+//        private final String authorName;
+//        private final String content;
+//
+//    }
 
-    }
+    public record writeMessageRequest (String authorName, String content) {}
+    public record writeMessageResponse (long id) {}
 
     @PostMapping("/writeMessage")
     @ResponseBody
-    public RsData<writeChatMessageResponse> writeMessage() {
+    public RsData<writeMessageResponse> writeMessage(@RequestBody writeMessageRequest req) {
 
-        ChatMessage message = new ChatMessage("홍길동", "안녕");
+        ChatMessage message = new ChatMessage(req.authorName, req.content);
 
         chatMessages.add(message);
 
-        return new RsData<>("S-1", "메세지가 작성됨", new writeChatMessageResponse(message.getId()) );
+        return new RsData<>("S-1", "메세지가 작성됨", new writeMessageResponse(message.getId()) );
     }
 
-
+    @GetMapping("/messages")
+    @ResponseBody
+    public RsData<List<ChatMessage>> messages() {
+        return new RsData<>("S-1",
+                "성공",
+                chatMessages
+        );
+    }
 }
